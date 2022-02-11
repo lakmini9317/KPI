f655139388a47ad133f9c258e604ca561edfed16
Lakmini Gunarathna <lakmini17gunarathna@gmail.com>
2022-02-11 15:33:20 +0530

    division charts

--- individual_report.php
+++ individual_report.php
@@ -1,58 +1,57 @@
 <?php
-
-$emp = $_POST['empcode'];
+$emp =(isset($_POST['empcode']) ? $_POST['empcode'] : '');
 
 $connect = mysqli_connect("localhost", "root", "admin", "kpi_eps"); 
 
-$query2 = "SELECT progq1, count(*) as number FROM duty_list2 where empcode =$emp GROUP BY progq1";  
+$query2 = "SELECT progq1, count(*) as number FROM duty_list2 where empcode=$emp GROUP BY progq1";  
 $result2 = mysqli_query($connect, $query2); 
 
-
+ 
 // $connect->close();
 ?> 
 
 <!DOCTYPE html>  
 <html>  
     <head>  
-           <title>Report Dashboard</title>  
-           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Report Dashboard</title>  
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> 
-           
-          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-          <script type="text/javascript" src="assets/dist/js/combocharts.js"></script>
-          <script type="text/javascript">            
-
-          google.charts.load('current', {'packages':['corechart']});  
-
-          google.charts.setOnLoadCallback(drawChart2);
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> 
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
           
-          function drawChart2()  
-          {  
-               var data = google.visualization.arrayToDataTable([  
-                         ['Duty', 'Progress'],  
-                         <?php  
-                         while($row = mysqli_fetch_array($result2))  
-                         {  
-                              echo "['".$row["progq1"]."', ".$row["number"]."],";  
-                         }  
-                         ?>  
-                    ]);
-
-               var options = {  
-                     title: 'Quater 01 Progress ',  
-                     is3D:true,  
-                     pieHole: 0.5  
-                    };  
-               var chart = new google.visualization.PieChart(document.getElementById('piechart2'));  
-               chart.draw(data, options);  
-          }          
-
-          </script> 
+        <script type="text/javascript"> 
+                  
+            google.charts.load('current', {'packages':['corechart']}); 
+            google.charts.setOnLoadCallback(drawChart);
           
+            function drawChart()  
+            {  
+                var data = google.visualization.arrayToDataTable([  
+                    ['Duty', 'Progress'],  
+                    <?php  
+                    while($row = mysqli_fetch_array($result2))  
+                    {  
+                        echo "['".$row["progq1"]."', ".$row["number"]."],";  
+                    }  
+                    ?>  
+                ]);
 
+                var options = {  
+                    title: 'Quater 01 Progress ',  
+                    is3D:true,  
+                    pieHole: 0.5  
+                };  
+
+                var chart = new google.visualization.PieChart(document.getElementById('piechart2'));  
+                chart.draw(data, options);  
+            }       
+
+        </script> 
+
+            
     </head>  
 
     <body>  
@@ -63,7 +62,7 @@ $result2 = mysqli_query($connect, $query2);
             <label>EMP Code</label>
             <input type="text" name="empcode"><br>
             <button type="submit" class="btn btn-md btn-primary" >Search</button>
-            <!-- <input type ="submit"> -->
+           
         </form> <br><br>
 
         <div class="row">
@@ -74,7 +73,7 @@ $result2 = mysqli_query($connect, $query2);
             </div>  <br>
         </div> 
         
-        
+         
 
 
     </body>  
--- individual_report2.php
+++ individual_report2.php
@@ -1,10 +1,18 @@
 <?php
 include 'db_connect.php';
 
-$emp = $_POST['empcode'];
+$emp =(isset($_POST['empcode']) ? $_POST['empcode'] : '');
+$division =(isset($_POST['division']) ? $_POST['division'] : '');
 
 $query="select empcode,noc,taskq1,progq1 from duty_list2 where empcode=$emp"; 
 $result=mysqli_query($conn,$query);
+
+$chartQuery = "SELECT * FROM duty_list2 where division=$division";
+$chartQueryRecords = mysqli_query($conn,$chartQuery);
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -13,55 +21,109 @@ $result=mysqli_query($conn,$query);
     <meta charset="UTF-8">
     <title>Individual Report</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+    <script type="text/javascript">
+
+      google.charts.load('current', {
+        'packages':['imagebarchart'],
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+      });
+
+      google.charts.setOnLoadCallback(drawRegionsMap);
+
+
+    function drawRegionsMap() {
+
+      var data = google.visualization.arrayToDataTable([
+          ['empcode', 'target', 'progress'],
+          <?php
+
+              while($row = mysqli_fetch_assoc($chartQueryRecords)){
+
+                  echo "['".$row['empcode']."',".$row['taskq1']."],".$row['progq1']."";
+              }
+          ?>
+      ]);
+
+
+      var options = {
+      };
+
+      var chart = new google.visualization.ImageBarChart(document.getElementById('divisionChart'));
+      chart.draw(data, options);
+
+    }
+
+</script>
+
+
+
+
+
+
+</head>
 </head>
 <body>
 
 
-<form action="" method="post">
+  <form action="" method="post">
              
-             <label>EMP Code</label>
-             <input type="text" name="empcode"><br>
-             <button type="submit" class="btn btn-md btn-primary" >Search</button>
-            
-         </form> <br><br>
+      <label>EMP Code</label>
+      <input type="text" name="empcode"><br>
+      <label>Division</label>
+      <input type="text" name="division"><br>
+      <button type="submit" class="btn btn-md btn-primary" >Search</button>
+    
+  </form> <br><br>
 
 
-<div class="container mt-2">
+  <div class="container mt-2">
     <div class="row">
-        <div class="col-md-12">
-            <div class="page-header">
-                <h2>Individual Progress</h2>
-            </div>
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">EMP Code</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Quarter 1</th>
-                  <th scope="col">Progress</th>
-                </tr>
-              </thead>
-              <tbody>
-                
-                <?php if ($result->num_rows > 0): ?>
-                <?php while($array=mysqli_fetch_row($result)): ?>
-                <tr>
-                    <th scope="row"><?php echo $array[0];?></th>
-                    <td><?php echo $array[1];?></td>
-                    <td><?php echo $array[2];?></td>
-                    <td><?php echo $array[3];?></td>
-                </tr>
-                <?php endwhile; ?>
-                <?php else: ?>
-                <tr>
-                   <td colspan="3" rowspan="1" headers="">No Data Found</td>
-                </tr>
-                <?php endif; ?>
-                <?php mysqli_free_result($result); ?>
-              </tbody>
-            </table>
+      <div class="col-md-12">
+        <div class="page-header">
+            <h2>Individual Progress</h2>
         </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">EMP Code</th>
+              <th scope="col">Name</th>
+              <th scope="col">Tasks Q1
+
+              </th>
+              <th scope="col">Progress</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+            <?php if ($result->num_rows > 0): ?>
+            <?php while($array=mysqli_fetch_row($result)): ?>
+            <tr>
+                <th scope="row"><?php echo $array[0];?></th>
+                <td><?php echo $array[1];?></td>
+                <td><?php echo $array[2];?></td>
+                <td><?php echo $array[3];?></td>
+            </tr>
+            <?php endwhile; ?>
+            <?php else: ?>
+            <tr>
+              <td colspan="3" rowspan="1" headers="">No Data Found</td>
+            </tr>
+            <?php endif; ?>
+            <?php mysqli_free_result($result); ?>
+          </tbody>
+        </table>
+      </div>
     </div>        
-</div>
+  </div> <br><br>
+
+  
+
+
+
+  <div id="divisionChart" style="width: 900px; height: 500px;"></div>
+
 </body>
 </html>
