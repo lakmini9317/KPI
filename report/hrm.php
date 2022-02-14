951ad2e2bd59fc26b35e42946cbf84cd03cea4a2
DeadlyXC <isurucso94@gmail.com>
2022-02-14 14:34:06 +0530

    table ui fixed

--- report/hrm.php
+++ report/hrm.php
@@ -1,5 +1,24 @@
 <?php include'../db_connect.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  	<script type = "text/javascript" src = "https://www.gstatic.com/charts/loader.js"> </script>
+	<script type = "text/javascript">
+		google.charts.load('current', {packages: ['corechart']});     
+    </script>
 
+
+
+	<title>Division Report</title>
+</head>
+<body>
+<h4>Division Report</h4> <br>
 
 <div class="col-lg-12">
 	<div class="card card-outline card-success">
@@ -22,7 +41,7 @@
 						<th colspan='2'>Quarter 01 </th>
 						<th colspan='2'>Quarter 02</th>
 						<th colspan='2'>Quarter 03</th>
-						
+						<th colspan='2'>Quarter 04</th>
 					</tr>
 
 					<tr>
@@ -30,6 +49,8 @@
 						<th>Target  </th>
 						<th>Progress(%)</th>
 						<th>Target  </th>
+						<th>Progress(%)</th>
+						<th>Target </th>
 						<th>Progress(%)</th>
 						<th>Target </th>
 						<th>Progress(%)</th>
--- report/landdm.php
+++ report/landdm.php
@@ -1,5 +1,24 @@
 <?php include'../db_connect.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  	<script type = "text/javascript" src = "https://www.gstatic.com/charts/loader.js"> </script>
+	<script type = "text/javascript">
+		google.charts.load('current', {packages: ['corechart']});     
+    </script>
 
+
+
+	<title>Division Report</title>
+</head>
+<body>
+<h4>Division Report</h4> <br>
 
 <div class="col-lg-12">
 	<div class="card card-outline card-success">
