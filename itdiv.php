ee578fd33cd9ace6993856f4af8d1f8ec20778ea
Lakmini Gunarathna <lakmini17gunarathna@gmail.com>
2022-02-14 12:47:37 +0530

    Update itdiv.php

--- itdiv.php
+++ itdiv.php
@@ -1,68 +1,82 @@
-<?php include'./db_connect.php' ?>
+<?php include'db_connect.php' ?>
 
 
-<div class="col-lg-12">
-	<div class="card card-outline card-success">
-		<div class="card-header">
-			<div class="card-tools">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Division Report</title>
+</head>
+<body>
+<h4>Division Report</h4> <br>
+	<div class="row">
+		
+		<div class="col-lg-12">
+		<table class="table tabe-hover table-bordered" id="list" >
+		<thead>
+			<tr class="text-center">
+
+				<th rowspan='2'>#</th>
+				<th rowspan='2'>EMP Code</th>
+				<th rowspan='2'>Name</th>
+				<th rowspan='2'>Designation</th>
+				<th rowspan='2'>Duty Framework</th>
+				<th colspan='2'>Quarter 01 </th>
+				<th colspan='2'>Quarter 02</th>
+				<th colspan='2'>Quarter 03</th>
 				
-			</div>
-		</div>	
+			</tr>
 
-		<div class="card-body">
-			<table class="table tabe-hover table-bordered" id="list">
-				<thead>
-					<tr class="text-center">
-
-						<th rowspan='2'>#</th>
-						<th rowspan='2'>EMP Code</th>
-						<th rowspan='2'>Name</th>
-						<th rowspan='2'>Designation</th>
-						<th rowspan='2'>Duty Framework</th>
-						<th colspan='2'>Quarter 01 </th>
-						<th colspan='2'>Quarter 02</th>
-						<th colspan='2'>Quarter 03</th>
-						
-					</tr>
-
-					<tr>
-						
-						<th>Target  </th>
-						<th>Progress(%)</th>
-						<th>Target  </th>
-						<th>Progress(%)</th>
-						<th>Target </th>
-						<th>Progress(%)</th>
-						
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					$i = 1;
-					
-					$qry = $conn->query("SELECT * FROM duty_list WHERE divisub='ITS Division'");
-					while($row= $qry->fetch_assoc()):
-					?>
-					<tr>
-						<th class="text-center"><?php echo $i++ ?></th>
-						<td><b><?php echo $row['empcode'] ?></b></td>
-						<td><b><?php echo ucwords($row['noc']) ?></b></td>
-						<td><b><?php echo $row['desig'] ?></b></td>
-						<td><b><?php echo $row['dutyfrm']  ?></b></td>
-						<td><?php echo $row['tutq1']  ?></td>
-						<td><b><?php echo $row['progq1p']  ?></b></td>
-						<td><?php echo $row['tutq2']  ?></td>
-						<td><b><?php echo $row['progq2p']  ?></b></td>
-						<td><?php echo $row['tutq3']  ?></td>
-						<td><b><?php echo $row['progq3p']  ?></b></td>
-						
-					</tr>	
-				<?php endwhile; ?>
-				</tbody>
-			</table>
+			<tr>
+				
+				<th>Target  </th>
+				<th>Progress(%)</th>
+				<th>Target  </th>
+				<th>Progress(%)</th>
+				<th>Target </th>
+				<th>Progress(%)</th>
+				
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$i = 1;
+			
+			$qry = $conn->query("SELECT * FROM duty_list WHERE divisub='ITS Division'");
+			while($row= $qry->fetch_assoc()):
+			?>
+			<tr>
+				<th class="text-center"><?php echo $i++ ?></th>
+				<td><b><?php echo $row['empcode'] ?></b></td>
+				<td><b><?php echo ucwords($row['noc']) ?></b></td>
+				<td><b><?php echo $row['desig'] ?></b></td>
+				<td><b><?php echo $row['dutyfrm']  ?></b></td>
+				<td><?php echo $row['tutq1']  ?></td>
+				<td><b><?php echo $row['progq1p']  ?></b></td>
+				<td><?php echo $row['tutq2']  ?></td>
+				<td><b><?php echo $row['progq2p']  ?></b></td>
+				<td><?php echo $row['tutq3']  ?></td>
+				<td><b><?php echo $row['progq3p']  ?></b></td>
+				
+			</tr>	
+		<?php endwhile; ?>
+		</tbody>
+	</table>
 		</div>
 	</div>
-</div>
+	
+			
+
+	
+
+</body>
+</html>
+
+
+
 <script>
 	$(document).ready(function(){
 		$('#list').dataTable()
