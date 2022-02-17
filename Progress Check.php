b5f187b11a95e3834f412564167190d38be85572
Lakmini Gunarathna <lakmini17gunarathna@gmail.com>
2022-02-17 10:01:48 +0530

    Update Progress Check.php

--- Progress Check.php
+++ Progress Check.php
@@ -5,6 +5,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<style>
+  table,tr,th,td{
+    border:1px solid black;
+    padding:10px;
+  }
+
+  table{
+    margin-bottom:20px;
+  }
+</style>
+
 </head>
 
 
