83e99753e4a544cffdf49e1fe0c0622e9f1cd4ef
DeadlyXC <isurucso94@gmail.com>
2022-05-31 11:51:47 +0530

    con issue fix

--- db_connect2.php
+++ db_connect2.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','excel')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','kpi eps')or die("Could not connect to mysql".mysqli_error($con));
